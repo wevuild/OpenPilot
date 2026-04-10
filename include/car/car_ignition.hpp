ebf25e10b64e8d75c9d839dfcdd46723ac1eec9c
allexanderbergmans <allexander.bergmans@student.elisa.be>
2026-04-10 10:42:14 +0000

    A: Added car_ignition header file, still WIP

---
+++ include/car/car_ignition.hpp
@@ -0,0 +1,49 @@
+/*
+ * Open-source auto-pilot software for robotics and vehicles.
+ * Control with precision, build with confidence.
+ *
+ * Copyright (C) 2026 WeVuild, Be
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Car ignition?
+
+#pragma once
+
+namespace car {
+
+enum class PowerState {
+	OFF,
+	SHORTAGE,
+	ON,
+	STARTING_ENGINE
+}
+
+class Igntion {
+public:
+	bool off();
+	bool shortage();
+	bool pwr_on();
+	bool start_engine();		// Should only be valid when you have a key that requires
+								// twisting, modern cars use a button, so this will be changed
+								// depending on the model.
+	PowerState state() const;
+
+private:
+	PowerState current_state = PowerState::OFF;
+	void set_state(PowerState new_state);
+};
+
+}
