9dc3d596d00a5c59c19e6ee7e7cce0daf32a0fd6
allexanderbergmans <allexander.bergmans@student.elisa.be>
2026-06-22 07:04:58 +0000

    * Dashboard ci

---
+++ src/ci_electric/dashboard.cpp
@@ -0,0 +1,34 @@
+/*
+ * Open-source auto-pilot software for robotics and vehicles.
+ * Control with precision, build with confidence.
+ *
+ * Copyright (C) 2026 WeVuild, Be
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "../../include/ci_electric/dashboard.hpp"
+#include "../../include/ci_electric/check.hpp"
+#include "../../include/ci_electric/pin.hpp"
+
+bool Dashboard::initialize() {
+    // Initialize dashboard components
+    status_ = DashboardStatus::STARTUP;
+    return true;
+}
+
+bool Dashboard::update() {
+    status_ = DashboardStatus::OK;
+    return true;
+}
